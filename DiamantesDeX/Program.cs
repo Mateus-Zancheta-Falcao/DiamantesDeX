6d5a018c04fa39b64f31d4ea5b4af683656e1234
Mateus Zancheta Falcao <mateuszancheta2005@gmail.com>
2023-03-16 13:33:41 -0300

    Add project files.

---
+++ DiamantesDeX/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamantesDeX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número ímpar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("O número digitado não é ímpar.");
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (Math.Abs(n / 2 - i) + Math.Abs(n / 2 - j) <= n / 2)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
